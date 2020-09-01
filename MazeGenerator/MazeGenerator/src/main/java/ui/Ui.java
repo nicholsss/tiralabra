cceb3c8c4102589ead85e276a984f82392d273de
nicholsss <mickyaw1995@gmail.com>
2020-09-01 14:35:53 +0300

    speed test fixing

--- MazeGenerator/MazeGenerator/src/main/java/ui/Ui.java
+++ MazeGenerator/MazeGenerator/src/main/java/ui/Ui.java
@@ -33,20 +33,23 @@ public class Ui {
                 System.out.println("3. Build Hunt And Kill Maze");
                 System.out.println("4. Build Aldous-Broder Maze");
                 System.out.println("5. Build All");
+                System.out.println("6. Get average build times within 10 build");
                 System.out.println("0. To EXIT");
                 System.out.print("Input value ");
                 read = sc.nextInt();
                 System.out.println("");
                 if (read == 1) {
                     System.out.println("");
+
                     long alku = System.nanoTime();
 
                     DfsRec dfs = new DfsRec(x, y);
                     System.out.println("*************************");
                     System.out.println("* Recursive Backtracker *");
                     System.out.println("*************************");
-                    dfs.draw();
+
                     long loppu = System.nanoTime();
+                    dfs.draw();
                     System.out.println("Time spent to build Recursive backtracker Maze " + ((loppu - alku) / 1e9) + " s");
 
                 }
@@ -59,8 +62,9 @@ public class Ui {
                     long alku1 = System.nanoTime();
 
                     BinaryTree binary = new BinaryTree(x, y);
-                    binary.draw();
+
                     long loppu1 = System.nanoTime();
+                    binary.draw();
                     System.out.println("Time spent to build Binary Tree Maze " + ((loppu1 - alku1) / 1e9) + " s");
                 }
                 System.out.println("");
@@ -71,8 +75,9 @@ public class Ui {
                     System.out.println("*************************");
                     System.out.println("*      Hunt And Kill    *");
                     System.out.println("*************************");
-                    hnk.draw();
+
                     long loppu = System.nanoTime();
+                    hnk.draw();
                     System.out.println("Time spent to build Hunt And Kill Maze " + ((loppu - alku) / 1e9) + " s");
 
                 }
@@ -84,8 +89,9 @@ public class Ui {
                     System.out.println("*************************");
                     System.out.println("*     Aldrous-Broder    *");
                     System.out.println("*************************");
-                    ab.draw();
+
                     long loppu = System.nanoTime();
+                    ab.draw();
                     System.out.println("Time spent to build Aldous-Broder Maze " + ((loppu - alku) / 1e9) + " s");
 
                 }
@@ -93,48 +99,83 @@ public class Ui {
                 System.out.println("");
                 if (read == 5) {
                     System.out.println("");
-                    long alku = System.nanoTime();
 
-                    DfsRec dfs = new DfsRec(x, y);
                     System.out.println("*************************");
                     System.out.println("* Recursive Backtracker *");
                     System.out.println("*************************");
-                    dfs.draw();
+                    long alku = System.nanoTime();
+                    DfsRec dfs = new DfsRec(x, y);
                     long loppu = System.nanoTime();
+                    dfs.draw();
 
                     System.out.println("");
                     System.out.println("********************");
                     System.out.println("* Binary Tree Maze *");
                     System.out.println("********************");
                     long alku1 = System.nanoTime();
-
                     BinaryTree binary = new BinaryTree(x, y);
-                    binary.draw();
                     long loppu1 = System.nanoTime();
+                    binary.draw();
 
-                    long alku2 = System.nanoTime();
-
-                    HuntnKill hnk = new HuntnKill(x, y);
                     System.out.println("");
                     System.out.println("*************************");
                     System.out.println("*      Hunt And Kill    *");
                     System.out.println("*************************");
-                    hnk.draw();
+                    long alku2 = System.nanoTime();
+                    HuntnKill hnk = new HuntnKill(x, y);
                     long loppu2 = System.nanoTime();
+                    hnk.draw();
 
-                    long alku3 = System.nanoTime();
-
-                    AldousBroder ab = new AldousBroder(x, y);
+                    System.out.println("");
                     System.out.println("*************************");
                     System.out.println("*     Aldrous-Broder    *");
                     System.out.println("*************************");
-                    ab.draw();
+                    long alku3 = System.nanoTime();
+                    AldousBroder ab = new AldousBroder(x, y);
                     long loppu3 = System.nanoTime();
+                    ab.draw();
+
                     System.out.println("");
                     System.out.println("Time spent to build Recursive backtracker Maze " + ((loppu - alku) / 1e9) + " s");
                     System.out.println("Time spent to build Binary Tree Maze " + ((loppu1 - alku1) / 1e9) + " s");
                     System.out.println("Time spent to build Hunt And Kill Maze " + ((loppu2 - alku2) / 1e9) + " s");
                     System.out.println("Time spent to build Aldous-Broder Maze " + ((loppu3 - alku3) / 1e9) + " s");
+                }
+                if (read == 6) {
+                    long dfsTotal = 0;
+                    long btTotal = 0;
+                    long hnkTotal = 0;
+                    long abTotal = 0;
+                    for (int i = 0; i < 10; i++) {
+                        long alku = System.nanoTime();
+                        DfsRec dfs = new DfsRec(x, y);
+                        long loppu = System.nanoTime();
+                        dfsTotal += (loppu - alku);
+                    }
+                    for (int i = 0; i < 10; i++) {
+                        long alku = System.nanoTime();
+                        BinaryTree bt = new BinaryTree(x, y);
+                        long loppu = System.nanoTime();
+                        btTotal += (loppu - alku);
+                    }
+                    for (int i = 0; i < 10; i++) {
+                        long alku = System.nanoTime();
+                        HuntnKill hnk = new HuntnKill(x, y);
+                        long loppu = System.nanoTime();
+                        hnkTotal += (loppu - alku);
+                    }
+                    for (int i = 0; i < 10; i++) {
+                        long alku = System.nanoTime();
+                        AldousBroder ab = new AldousBroder(x, y);
+                        long loppu = System.nanoTime();
+                        abTotal += (loppu - alku);
+                    }
+
+                    System.out.println("dfs: " + (dfsTotal / 10) / 1e9 + "s");
+                    System.out.println("bt: " + (btTotal / 10) / 1e9);
+                    System.out.println("hnk: " + (hnkTotal / 10) / 1e9);
+                    System.out.println("ab: " + (abTotal / 10) / 1e9);
+
                 }
 
                 if (read == 0) {
